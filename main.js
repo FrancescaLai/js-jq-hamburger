ad2496b99c5114db97189d4ef5dc1f82712a5274
FrancescaLai <francescalai1690@gmail.com>
2021-03-11 16:39:12 +0100

    Random js feature added

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ $(".hamburger-menu .close").click(
     $(".hamburger-menu").removeClass("active");
   }
 )
+
+
+var testo = $(".images p");
+var immagine = $(".images img")
+var visible = true;
+
+testo.mouseover(
+  function(){
+    if(visible == true){
+      immagine.fadeOut("slow");
+      visible = false;
+    } else {
+      immagine.fadeIn("fast");
+      visible = true;
+    }
+  }
+)
